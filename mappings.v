37a9ef05b4fa97211deb1d693eeedffd0d36c175
Frédéric Blanqui <frederic.blanqui@inria.fr>
2025-08-01 15:04:27 +0200

    wip

--- mappings.v
+++ mappings.v
@@ -39,6 +39,12 @@ Proof. rewrite h1, h2. reflexivity. Qed.
 Lemma EQ_MP {p q : Prop} (e : p = q) (h : p) : q.
 Proof. rewrite <- e. apply h. Qed.
 
+Lemma TRANS {a : Type'} {x y z : a} (xy : x = y) (yz : y = z) : x = z.
+Proof. exact (@EQ_MP (x = y) (x = z) (@MK_COMB a Prop (@eq a x) (@eq a x) y z (@eq_refl (a -> Prop) (@eq a x)) yz) xy). Qed.
+
+Lemma SYM {a : Type'} {x y : a} (xy : x = y) : y = x.
+Proof. exact (@EQ_MP (x = x) (y = x) (@MK_COMB a Prop (@eq a x) (@eq a y) x x (@MK_COMB a (a -> Prop) (@eq a) (@eq a) x y (@eq_refl (a -> a -> Prop) (@eq a)) xy) (@eq_refl a x)) (@eq_refl a x)). Qed.
+
 (****************************************************************************)
 (* Proof of some natural deduction rules. *)
 (****************************************************************************)
